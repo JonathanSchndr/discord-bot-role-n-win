4e2016d7acb4e1f6cb164c73237705f2be924753
Jonathan Schneider <jonathan@jott.media>
2023-09-27 18:19:18 +0200

    update file

--- index.mjs
+++ index.mjs
@@ -1,9 +1,20 @@
 import { Client, GatewayIntentBits } from 'discord.js';
+
 const client = new Client({
-    intents: [GatewayIntentBits.Flags.Guilds, GatewayIntentBits.Flags.GuildMessages, GatewayIntentBits.Flags.MessageContent]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
 });
 
-const token = 'DEIN_DISCORD_BOT_TOKEN';
+const token = process.env.DISCORD_BOT_TOKEN;
+
+// Map zum Speichern der Benutzer-IDs und der Zeit, zu der sie zuletzt gewürfelt haben
+const userTimestamps = new Map();
+
+// Map von verfügbaren Amazon Keys
+const amazonKeys = new Map([
+    ['key1', 'AMAZON_KEY_1'],
+    ['key2', 'AMAZON_KEY_2'],
+    // ... Weitere Keys
+]);
 
 client.once('ready', async () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -23,9 +34,36 @@ client.on('interactionCreate', async (interaction) => {
 
     const { commandName } = interaction;
     if (commandName === 'role') {
+        const userId = interaction.user.id;
+
+        // Überprüfe, ob der Benutzer heute bereits gewürfelt hat
+        const lastTimestamp = userTimestamps.get(userId);
+        const now = Date.now();
+        const oneDay = 24 * 60 * 60 * 1000;
+
+        if (lastTimestamp && now - lastTimestamp < oneDay) {
+            interaction.reply('Du darfst nur einmal am Tag würfeln! Versuche es morgen erneut.');
+            return;
+        }
+
+        // Speichere den aktuellen Zeitstempel für diesen Benutzer
+        userTimestamps.set(userId, now);
+
+        // Führe den Würfelwurf durch
         const number = Math.floor(Math.random() * 100) + 1;
         if (number === 66) {
-            interaction.reply('Glückwunsch! Du hast eine 66 gewürfelt und gewinnst einen Amazon Key!');
+            if (amazonKeys.size === 0) {
+                interaction.reply('Leider sind alle Amazon Keys bereits vergeben.');
+                return;
+            }
+
+            // Wähle einen zufälligen Amazon Key aus der Map und entferne ihn
+            const keyIndex = Math.floor(Math.random() * amazonKeys.size);
+            const key = Array.from(amazonKeys.values())[keyIndex];
+            const keyId = Array.from(amazonKeys.keys())[keyIndex];
+            amazonKeys.delete(keyId);
+
+            interaction.reply(`Glückwunsch! Du hast eine 66 gewürfelt und gewinnst den Amazon Key: ${key}`);
         } else {
             interaction.reply(`Du hast eine ${number} gewürfelt. Leider kein Gewinn, versuche es erneut!`);
         }
