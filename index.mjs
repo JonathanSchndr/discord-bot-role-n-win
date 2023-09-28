9363271f7862d69737137781ef53d221722f90dc
Jonathan Schneider <jonathan@jott.media>
2023-09-28 09:20:56 +0200

    update file

--- index.mjs
+++ index.mjs
@@ -1,19 +1,19 @@
 import { Client, GatewayIntentBits } from 'discord.js';
 
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.DirectMessages]
 });
 
 const token = process.env.DISCORD_BOT_TOKEN;
 
-// Map zum Speichern der Benutzer-IDs und der Zeit, zu der sie zuletzt gewürfelt haben
-const userTimestamps = new Map();
+// Map to store user IDs and the date on which they last rolled the dice
+const userDates = new Map();
 
-// Map von verfügbaren Amazon Keys
+// Map of available Amazon Keys
 const amazonKeys = new Map([
     ['key1', 'AMAZON_KEY_1'],
     ['key2', 'AMAZON_KEY_2'],
-    // ... Weitere Keys
+    // ... More keys
 ]);
 
 client.once('ready', async () => {
@@ -23,8 +23,8 @@ client.once('ready', async () => {
     for (const guild of guilds.values()) {
         const commands = guild.commands;
         await commands.create({
-            name: 'role',
-            description: 'Wirf einen Würfel und gewinne vielleicht einen Amazon Key!',
+            name: 'roll',
+            description: 'Roll a dice and you might win an Amazon Key!',
         });
     }
 });
@@ -32,40 +32,49 @@ client.once('ready', async () => {
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
 
-    const { commandName } = interaction;
-    if (commandName === 'role') {
-        const userId = interaction.user.id;
+    const { commandName, user } = interaction;
+    if (commandName === 'roll') {
+        const userId = user.id;
 
-        // Überprüfe, ob der Benutzer heute bereits gewürfelt hat
-        const lastTimestamp = userTimestamps.get(userId);
-        const now = Date.now();
-        const oneDay = 24 * 60 * 60 * 1000;
+        // Check if the user has already rolled the dice today
+        const currentDate = (new Date()).toDateString();
+        const lastDate = userDates.get(userId);
 
-        if (lastTimestamp && now - lastTimestamp < oneDay) {
-            interaction.reply('Du darfst nur einmal am Tag würfeln! Versuche es morgen erneut.');
+        if (lastDate && lastDate === currentDate) {
+            interaction.reply('You can only roll once per day! Try again tomorrow.');
             return;
         }
 
-        // Speichere den aktuellen Zeitstempel für diesen Benutzer
-        userTimestamps.set(userId, now);
+        // Store the current date for this user
+        userDates.set(userId, currentDate);
 
-        // Führe den Würfelwurf durch
+        // Determine the winning number for today
+        const daySeed = new Date().getDate();
+        const winningNumber = Math.floor(Math.random() * daySeed * 100) % 100 + 1;
+
+        // Roll the dice
         const number = Math.floor(Math.random() * 100) + 1;
-        if (number === 66) {
+        if (number === winningNumber) {
             if (amazonKeys.size === 0) {
-                interaction.reply('Leider sind alle Amazon Keys bereits vergeben.');
+                interaction.reply('Unfortunately, all Amazon Keys have already been claimed.');
                 return;
             }
 
-            // Wähle einen zufälligen Amazon Key aus der Map und entferne ihn
+            // Select a random Amazon Key from the Map and remove it
             const keyIndex = Math.floor(Math.random() * amazonKeys.size);
             const key = Array.from(amazonKeys.values())[keyIndex];
             const keyId = Array.from(amazonKeys.keys())[keyIndex];
             amazonKeys.delete(keyId);
 
-            interaction.reply(`Glückwunsch! Du hast eine 66 gewürfelt und gewinnst den Amazon Key: ${key}`);
+            // Send the Amazon Key via Direct Message
+            try {
+                await user.send(`Congratulations! You rolled the winning number ${winningNumber} and won the Amazon Key: ${key}`);
+                interaction.reply('You won! Check your Direct Messages for your Amazon Key.');
+            } catch (e) {
+                interaction.reply('I couldn\'t send you a Direct Message. Make sure you allow Direct Messages on this server.');
+            }
         } else {
-            interaction.reply(`Du hast eine ${number} gewürfelt. Leider kein Gewinn, versuche es erneut!`);
+            interaction.reply(`You rolled a ${number}. Better luck next time!`);
         }
     }
 });
